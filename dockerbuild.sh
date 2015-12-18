45f3a2eead611a660811f9e1c471790ca5d59409
Daniel Gunnlaugsson <daniel.thor.gunnlaugsson@gmail.com>
2015-12-18 15:35:30 +0000

    Mirrored dockerbuild.sh changes from Gulli

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,21 +5,64 @@ rm -rf ./dist
 
 export DISPLAY=:0
 
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
 echo Building app
 grunt
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Grunt build failed with exit code " $rc
+    exit $rc
+fi
+
+cat > ./dist/githash.txt <<_EOF_
+$GIT_COMMIT
+_EOF_
+
+cat > ./dist/public/version.html << _EOF_
+<!doctype html>
+<head>
+   <title>TicTacToe version information</title>
+</head>
+<body>
+   <span>Origin:</span> <span>$GITHUB_URL</span>
+   <span>Revision:</span> <span>$GIT_COMMIT</span>
+   <p>
+   <div><a href="$GITHUB_URL/commits/$GIT_COMMIT">History of current version</a></div>
+</body>
+_EOF_
 
 cp ./Dockerfile ./dist/
 
 cd dist
 npm install --production
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+if [[ $rc != 0 ]] ; then
+    echo "NPM install failed with exit code " $rc
+    exit $rc
+fi
 
 echo Building docker image
 docker build -t dthor/tictactoe .
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+if [[ $rc != 0 ]] ; then
+    echo "NPM install failed with exit code " $rc
+    exit $rc
+fi
 
 echo Pushing docker image
 docker push dthor/tictactoe
+
+if [[ $rc != 0 ]] ; then
+    echo "NPM install failed with exit code " $rc
+    exit $rc
+fi
 
 echo "Done"
